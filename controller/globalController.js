362bad22322d84554081e4970e1f7981c006d907
LazyMG <cbfmark@gmail.com>
2024-05-16 19:56:06 +0900

    24.05.16

--- controller/globalController.js
+++ controller/globalController.js
@@ -1,9 +1,16 @@
 import Music from "../models/Music";
 import User from "../models/User";
 
-export const home = (req, res) => {
+export const home = async (req, res) => {
   console.log("home");
-  return res.send("Home");
+  //음악들 불러오기
+  const allMusics = await Music.find({});
+  const recentMusics = allMusics.sort(() => 0.5 - Math.random()).slice(0, 6);
+  const recommendMusics = allMusics
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 12);
+  //console.log(recentMusics, recommendMusics);
+  return res.json(allMusics);
 };
 
 export const search = (req, res) => {
--- server.js
+++ server.js
@@ -3,11 +3,22 @@ import { globalRouter } from "./router/globalRouter";
 import { userRouter } from "./router/userRouter";
 import { musicRouter } from "./router/musicRouter";
 import { apiRouter } from "./router/apiRouter";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true })); //req.body 사용시 필요
 app.use(express.json());
+
+// app.use(function (req, res) {
+//   res.header("Access-Control-Allow-Origin", "https://cedarsojt.store");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+// });
+
+app.use(cors());
 
 app.use("/", globalRouter);
 app.use("/music", musicRouter);
